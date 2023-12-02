ffd42a490b32f6fda1a1949f1755d99159d33a42
Carles Capellas <capellas.carles@gmail.com>
2023-12-02 17:12:19 +0100

    Add nyc code coverage and reporting

--- projects/@typed-web-api/client/source/build-url.step.ts
+++ projects/@typed-web-api/client/source/build-url.step.ts
@@ -2,12 +2,12 @@ import { Before, Given, Then, When } from '@cucumber/cucumber';
 import { expect } from 'chai';
 import { buildUrl, BuildUrlOptions } from './build-url';
 
-let options: BuildUrlOptions;
+let options: undefined | BuildUrlOptions;
 let path: undefined | string;
 let result: undefined | string;
 
 Before(() => {
-  options = {};
+  options = undefined;
   path = undefined;
   result = undefined;
 });
@@ -17,14 +17,23 @@ Given(/a path "(.*)"/, (_path: string) => {
 });
 
 Given(/a urlPrefix "(.*)"/, (urlPrefix: string) => {
+  if (!options) {
+    options = {};
+  }
   options.urlPrefix = urlPrefix;
 });
 
 Given(/a baseUrl "(.*)"/, (baseUrl: string) => {
+  if (!options) {
+    options = {};
+  }
   options.baseUrl = baseUrl;
 });
 
 Given(/a "(.*)" URL parameter with value "(.*)"/, (paramName: string, paramValue: string) => {
+  if (!options) {
+    options = {};
+  }
   if (!options.urlParams) {
     options.urlParams = {};
   }
@@ -34,6 +43,9 @@ Given(/a "(.*)" URL parameter with value "(.*)"/, (paramName: string, paramValue
 Given(
   /a "(.*)" query string parameter with value "(.*)"/,
   (paramName: string, paramValue: string) => {
+    if (!options) {
+      options = {};
+    }
     if (!options.queryString) {
       options.queryString = {};
     }
