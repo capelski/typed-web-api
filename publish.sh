2775f8d3e8a8ac6ce8b32c79ab4cc7452c5db653
Carles Capellas <capellas.carles@gmail.com>
2024-06-18 14:54:12 +0200

    Simplify publish.sh

--- publish.sh
+++ publish.sh
@@ -1,45 +1,34 @@
 # Meant for documentation purposes, since the publish step requires 2FA validation
 
-cd projects/@typed-web-api/common
-npm version patch # patch, minor or major
-npm publish
-cd ../../..
+npm run test # Make sure all tests pass before publishing
 
-cd projects/@typed-web-api/client
-npm i -S @typed-web-api/common@latest
-npm version patch # patch, minor or major
-npm publish
-cd ../../..
+npm version patch -w @typed-web-api/common # patch, minor or major
+npm publish -w @typed-web-api/common
 
-cd projects/@typed-web-api/express 
-npm i -S @typed-web-api/common@latest
-npm version patch # patch, minor or major
-npm publish
-cd ../../..
+npm i -S @typed-web-api/common@latest -w @typed-web-api/client
+npm version patch -w @typed-web-api/client # patch, minor or major
+npm publish -w @typed-web-api/client
+ 
+npm i -S @typed-web-api/common@latest -w @typed-web-api/express
+npm version patch -w @typed-web-api/express # patch, minor or major
+npm publish -w @typed-web-api/express
 
-cd projects/@typed-web-api/nestjs 
-npm i -S @typed-web-api/common@latest
-npm version patch # patch, minor or major
-npm publish
-cd ../../..
+npm i -S @typed-web-api/common@latest -w @typed-web-api/nestjs 
+npm version patch -w @typed-web-api/nestjs # patch, minor or major
+npm publish -w @typed-web-api/nestjs
 
-cd projects/sample-common 
-npm i -S @typed-web-api/common@latest
-cd ../../..
+npm i -S @typed-web-api/common@latest -w sample-common
+npm i -S @typed-web-api/client@latest -w sample-client 
+npm i -S @typed-web-api/express@latest -w sample-server-express
+npm i -S @typed-web-api/nestjs@latest -w sample-server-nestjs
 
-cd projects/sample-client 
-npm i -S @typed-web-api/client@latest
-cd ../../..
+npm i --package-lock-only # Necessary to adjust package-lock.json after installs
 
-cd projects/sample-server-express
-npm i -S @typed-web-api/express@latest
-cd ../../..
-
-cd projects/sample-server-nestjs
-npm i -S @typed-web-api/nestjs@latest
-cd ../../..
-
-npm i # Necessary to adjust package-lock.json after installs
+# Build the sample projects to make sure the new versions don't break the api
+npm run build -w sample-common
+npm run build -w sample-client
+npm run build -w sample-server-express
+npm run build -w sample-server-nestjs
 
 git add .
 git commit -m "version X.Y.Z"
